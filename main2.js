ab80288261a546a1c503b7326b03e90239d0c20c
Shobi <ppshobi@gmail.com>
2017-06-03 14:57:48 +0530

    Hiding message model using vue funciton

--- main2.js
+++ main2.js
@@ -1,16 +1,26 @@
 Vue.component('message', {
 	props:['title', 'body'],
+	data(){
+		return {
+			isVisible:true
+		}
+	},
 	template:
-	`	<article class="message">
+	`	<article class="message" v-show="isVisible">
 		  <div class="message-header">
 		    <p>{{ title }}</p>
-		    <button class="delete"></button>
+		    <button class="delete" @click="hideMessage"></button>
 		  </div>
 		  <div class="message-body">
 		    {{ body }}
 		  </div>
 		</article>
-	`
+	`,
+	methods:{
+		hideMessage(){
+			this.isVisible=false
+		}
+	}
 })
 
 app = new Vue({
