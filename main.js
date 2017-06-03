61b0e5112175f7e0c4c03facd533d0635cf54ba5
Shobi <ppshobi@gmail.com>
2017-06-03 14:18:16 +0530

    Templating using vue component

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
+
+Vue.component('task-list', {
+	template: '<div><task v-for="task in tasks" v-text="task.description"></task></div>',
+
+	data(){
+		return {
+			tasks:[
+				{description:"Goto Store", completed:true},
+				{description:"Get Food", completed:false},
+				{description:"Clean Room", completed:false}
+			]
+		}
+	}
+
+});
+
 Vue.component('task', {
 	template: '<li><slot></slot></li>'
 });
-Vue.component('task-title', {
-	template: '<h3><slot></slot></h3>'
-});
-
 
 app = new Vue({
 	el:'#root',
