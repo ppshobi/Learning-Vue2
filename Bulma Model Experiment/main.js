0042252879d471ef037560ac47c94dbdbe110f2a
Shobi <ppshobi@gmail.com>
2017-06-04 17:08:31 +0530

    Content placed inside the html using slot

--- Bulma Model Experiment/main.js
+++ Bulma Model Experiment/main.js
@@ -3,14 +3,7 @@ template:`<div class="modal is-active">
 		  <div class="modal-background"></div>
 		  <div class="modal-content">
 		  	<div class="box">
-		  		<p>
-		    	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		    	</p>
+		  		<slot></slot>
 		  	</div>
 		    
 		  </div>
