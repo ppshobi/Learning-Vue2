a5b5a7b54c74fabc58e04bfa3b8ad8a6b9d2cd38
Shobi <ppshobi@gmail.com>
2017-06-04 17:06:27 +0530

    Component Communication and custom events

--- Bulma Model Experiment/main.js
+++ Bulma Model Experiment/main.js
@@ -1,3 +1,34 @@
+Vue.component('modal', {
+template:`<div class="modal is-active">
+		  <div class="modal-background"></div>
+		  <div class="modal-content">
+		  	<div class="box">
+		  		<p>
+		    	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		    	</p>
+		  	</div>
+		    
+		  </div>
+		  <button class="modal-close" @click="$emit('close')"></button>
+		</div>`
+})
+
 app = new Vue({
-	el:'#roo1t'
+	el:'#root',
+	data:{
+		showModal: false
+	},
+	methods:{
+		showModalnow(){
+			this.showModal=true;
+		},
+		closeModal(){
+			this.showModal=false;
+		}
+	}
 })
