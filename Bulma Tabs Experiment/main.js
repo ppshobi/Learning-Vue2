8d0d2a877832ff4731e5ccb2542e4ac7881c8686
Shobi <ppshobi@gmail.com>
2017-06-05 22:50:57 +0530

    default tab selected automatically

---
+++ Bulma Tabs Experiment/main.js
@@ -0,0 +1,50 @@
+Vue.component('tabs', {
+
+	 template:`
+	 	<div>
+		 	<div class="tabs">
+	  			<ul>
+	    			<li v-for="tab in tabs" :class="{ 'is-active': tab.selected }">
+	    				<a href="#"> {{ tab.name }} </a>
+	    			</li>
+	  			</ul>
+			</div>
+
+			<div class="tab-details">
+				<slot></slot>
+			</div>
+		</div>
+	`,
+	data(){
+		return {
+			tabs: []
+		}
+	},
+	created(){
+		this.tabs=this.$children;
+	}
+
+})
+
+Vue.component('tab', {
+
+	template:`
+	 	<div><slot></slot></div>
+	`,
+	props:{
+		name:{ required: true },
+		selected:{ default:false }
+	}
+
+})
+
+
+app = new Vue({
+	el:'#root',
+	data:{
+		
+	},
+	methods:{
+		
+	}
+})
