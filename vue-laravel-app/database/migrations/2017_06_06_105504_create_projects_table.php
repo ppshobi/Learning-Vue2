321798ad171329f026bfe6bc5853784a610ed9c8
Shobi <ppshobi@gmail.com>
2017-06-08 21:02:40 +0530

    Fix bug

--- vue-laravel-app/database/migrations/2017_06_06_105504_create_projects_table.php
+++ vue-laravel-app/database/migrations/2017_06_06_105504_create_projects_table.php
@@ -16,7 +16,7 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',30);
-            $table->text('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
