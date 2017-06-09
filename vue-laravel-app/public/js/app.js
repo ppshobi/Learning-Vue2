3677b4fd0c9915bce28b961bff68de666eb1ca1d
Shobi <ppshobi@gmail.com>
2017-06-09 14:12:50 +0530

    input validation using vue

--- vue-laravel-app/public/js/app.js
+++ vue-laravel-app/public/js/app.js
@@ -1,16 +1,33 @@
 
+class Error{
+
+	constructor(){
+		this.errors = {};
+	}
+
+	get(field){
+		if (this.errors[field]) {
+			return this.errors[field][0];
+		}
+	}
+
+	record(errors){
+		this.errors = errors;
+	}
+}
+
 const app = new Vue({
     el: '#root',
     data:{
     	name:'',
     	description:'',
-    	errors:{}
+    	errors:new Error()
     },
     methods:{
     	onSubmit(){
     		axios.post('/projects',this.$data)
     		.then(response => alert("Success"))
-    		.catch(error => this.errors = error.response.data)
+    		.catch(error => this.errors.record(error.response.data))
     	}
     }
 });
